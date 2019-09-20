be22d356cf367a06b44b9a3f662a0e13b497c08a
Sergey Makeev <makeev@freeconferencecall.com>
2019-09-20 12:52:16 +0300

    initial code

---
+++ source/SwiftAnonymousClass.swift
@@ -0,0 +1,11 @@
+//
+//  SwiftAnonymousClass.swift
+//  SwiftAnonymousClass
+//
+//  Created by Sergey Makeev on 20/09/2019.
+//  Copyright © 2019 Sergey Makeev. All rights reserved.
+//
+//
+public func _new<Type>(_ objectCreator:()->Type) -> Type {
+    return objectCreator()
+}
